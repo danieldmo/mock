d88e9e27585586b204000ecacba6ec7520a54a4d
Daniel <daniel@Daniels-MacBook-Pro.local>
2015-03-13 16:12:43 +0000

    stuff

---
+++ nodejs-campaign-mock/api/controllers/KeywordGroupController.js
@@ -0,0 +1,11 @@
+/**
+ * KeywordGroupController
+ *
+ * @description :: Server-side logic for managing Keywordgroups
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
